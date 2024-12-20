0323c89451e633ad4c32ec2afb2edd6492b339dd
Frederik Vos <frederik.vos@linvirt.nl>
2024-12-20 14:07:18 +0100

    vterm multi added

--- modules/lv-shell.el
+++ modules/lv-shell.el
@@ -1,13 +1,12 @@
 ;; Shell
-(defun my-exec-path-from-shell-initialize ()
-     (when (memq window-system '(mac ns x))
-       (exec-path-from-shell-initialize)))
-(use-package exec-path-from-shell
-  :straight t
-  :init
-  (add-hook 'after-init-hook 'my-exec-path-from-shell-initialize)
-)
+(setq-default explicit-shell-file-name "/bin/bash")
 
+;; Path Variable
+(use-package exec-path-from-shell
+  :init
+  (exec-path-from-shell-initialize))
+
+;; VTERM
 (use-package vterm
   :config
   (blink-cursor-mode 0)
@@ -23,6 +22,13 @@
     (let ((default-directory (project-root (project-current t))))
       (vterm "*vterm*"))))
 
+(use-package multi-vterm
+  :init
+  (setq vterm-always-compile-module t)
+  :requires vterm
+)
+
+;; ESHELL
 (use-package eshell
   :straight (:type built-in)
   :init
@@ -58,5 +64,27 @@
                    (visual-line-mode +1))))
 
 (add-to-list 'term-file-aliases '("foot" . "xterm"))
+
+(straight-use-package
+ '(eat :type git
+       :host codeberg
+       :repo "akib/emacs-eat"
+       :files ("*.el" ("term" "term/*.el") "*.texi"
+               "*.ti" ("terminfo/e" "terminfo/e/*")
+               ("terminfo/65" "terminfo/65/*")
+               ("integration" "integration/*")
+               (:exclude ".dir-locals.el" "*-tests.el"))))
+
+;; Shortcuts
+(global-set-key (kbd "C-c t c") 'vterm)
+(global-set-key (kbd "C-c t v") 'multi-vterm)
+(global-set-key (kbd "C-c t n") 'multi-vterm-next)
+(global-set-key (kbd "C-c t p") 'multi-vterm-prev)
+(global-set-key (kbd "C-c t r") 'multi-vterm-rename-buffer)
+(global-set-key (kbd "C-c t d") 'multi-vterm-dedicated-open)
+(global-set-key (kbd "C-c t j") 'multi-vterm-project)
+(global-set-key (kbd "C-c t q") 'multi-vterm-dedicated-close)
+(global-set-key (kbd "C-c t t") 'multi-vterm-dedicated-toggle)
+(global-set-key (kbd "C-c t s") 'multi-vterm-dedicated-select)
 
 (provide 'lv-shell)
