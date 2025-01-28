8b5d910c043fa04ac163ea4388cf18895f89fb42
Frederik Vos <inktvis75@gmail.com>
2025-01-28 20:47:21 +0100

    small ansible fixes

--- modules/lv-ansible.el
+++ modules/lv-ansible.el
@@ -7,31 +7,7 @@
   (prog-mode)
   (yaml-ts-mode)
   (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
-  :custom
-  (ansible-vault-password-file "~/.ansible-vault-pwd")
-
-  :bind
-  (:map ansible-key-map
-        ("C-c v" . rr/set-ansible-vault-mimipass-pwd)
-        ("C-c C-d" . ansible-doc))
-
-  :config
-  (add-hook 'ansible-hook 'ansible-auto-decrypt-encrypt)
-  (defun rr/write-string (string file)
-    (with-temp-buffer
-      (insert string)
-      (write-region (point-min) (point-max) file)))
-
-  (defun rr/set-ansible-vault-mimipass-pwd ()
-    "Choose which mimipass password to be used for ansible vault."
-    (interactive)
-    (rr/write-string (format "#!/bin/bash\nmimipass get %s"
-                             (rr/helm-mimipass))
-                     ansible-vault-password-file)
-    (chmod ansible-vault-password-file
-           (string-to-number "700" 8)))
 )
-
 
 (use-package ansible-doc
   :ensure t
