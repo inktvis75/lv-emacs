27ee0a35f507bab8f179768ae3656c8700c25fd3
Frederik Vos <inktvis75@gmail.com>
2025-02-26 21:27:09 +0100

    kubed keybinding

--- modules/lv-containers.el
+++ modules/lv-containers.el
@@ -7,8 +7,16 @@
 
 (use-package kubed
   :ensure t
+  :custom
+  (keymap-global-set "s-k" #'kubed-transient)
+  (keymap-set kubed-prefix-map "k" #'kubed-transient)
   :config
   (setq kubed-kubectl-program "/opt/homebrew/bin/kubectl")
+)
+
+(use-package k8s-mode
+  :ensure t
+  :hook (k8s-mode . yas-minor-mode)
 )
 
 (provide 'lv-containers)
